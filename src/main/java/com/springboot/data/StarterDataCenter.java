0e7778eb0adf7e4b42e2aca06738db6d727aa8ea
dreature1328 <483833885@qq.com>
2023-04-01 22:57:49 +0800

    '优化项目，并将原先模板分离成增删改查模板和迁移模板'

--- src/main/java/com/springboot/data/StarterDataCenter.java
+++ src/main/java/com/springboot/data/StarterDataCenter.java
@@ -1,31 +1,48 @@
 package com.springboot.data;
 
+import com.springboot.data.common.pojo.Data;
 import com.springboot.data.common.utils.SpringContextUtils;
 import com.springboot.data.service.DataService;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SpringBootApplication
 @MapperScan("com.springboot.data.mapper")
 @Import(SpringContextUtils.class)
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling   // 2.开启定时任务
 public class StarterDataCenter {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(StarterDataCenter.class, args);
-	}
 
-//	@Autowired
-//	private DataService dataService;
-//
-//	@Scheduled(cron ="0 */5 * * * ?")
-//	public void pageInsertOrUpdateDataOnSchedule() throws Exception {
-//		dataService.pageInsertOrUpdateData();
-//	}
+		// 下面是直接在启动类里增删改查的示例
+		DataService dataService = SpringContextUtils.getBean(DataService.class);
+		Data data1 = new Data("id1","value11","value12");
+		Data data2 = new Data("id2","value21","value22");
+		Data data3 = new Data("id3","value31","value32");
+
+		Data[] dataArray = {data1, data2, data3};
+
+		List<Data> dataList = new ArrayList<Data>(){{
+			add(data1);
+			add(data2);
+			add(data3);
+		}};
+
+		List<String> idList = new ArrayList<String>(){{
+			add("id1");
+			add("id2");
+			add("id3");
+		}};
+
+		// 直接调用 Service 层函数
+		dataService.insertData(data1);
+		dataService.insertData(data1,data2);
+		dataService.updateData(dataArray);
+		dataService.batchInsertOrUpdateData(dataList);
+	}
 }
